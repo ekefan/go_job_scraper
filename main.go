127581bca3bbb849f309341990cf55f7e92a1898
Ebenezer <eebenezer949@gmail.com>
2024-07-22 19:43:22 +0100

    main: load envs before starting server

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/ekefan/go_job_scraper/handler"
+import (
+	"log"
+	"github.com/ekefan/go_job_scraper/handler"
+)
 
 // The entire programs starts running here
 func main() {
-	
+	errLoadingEnv := handler.LoadDotEnv("job.env")
+	if errLoadingEnv != nil {
+		log.Printf("Error loading the env variables")
+		return
+	}
 	handler.RunBotServer()
 
 }
